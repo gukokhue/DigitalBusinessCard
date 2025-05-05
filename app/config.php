34b3da3e7a8a6dd48edf0a9fe79fe4b7811e1187
Yusuf Arslan <hello@yusufarslan.dev>
2025-05-05 21:12:33 +0300

    config improved

--- app/config.php
+++ app/config.php
@@ -1,16 +1,27 @@
 <?php
 
+// Error Log
+ini_set( 'display_errors', 1 );
+ini_set( 'display_startup_errors', 1 );
+error_reporting( E_ALL );
+
 $database = "digitalbusinesscard";
 $username = "root";
 $password = "rootpass";
 $host = "digitalbusinesscard-db";
 
-# Check connection
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Hataları yakala
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // Fetch tipi
+    PDO::ATTR_EMULATE_PREPARES   => false,                  // Native prepared statements kullan
+];
+
+// Check connection
 try {
-  $conn = new PDO( "mysql:host=$host;dbname=$database", $username, $password );
-  // set the PDO error mode to exception
-  $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-  echo "Connected successfully!";
+    $conn = new PDO( "mysql:host=$host;dbname=$database", $username, $password, $options );
+    // set the PDO error mode to exception
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    echo "Connected successfully!";
 } catch( PDOException $e ) {
-  echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
 }
